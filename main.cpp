eac596f2864c89a407c6f6b729477995ad1513a0
Ahmet Ersoz <abersoz@gmail.com>
2018-06-05 16:27:25 +0300

    random tangling implemented.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,48 @@
+#include "stdafx.h"
 #include <iostream>
 #include "OBJ_Loader.h"
 #include <Eigen/Dense>
-//#include <ceres/ceres.h>
+#include <ceres/ceres.h>
 
 using namespace std;
 using namespace Eigen;
-/*
 using ceres::DynamicAutoDiffCostFunction;
 using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solver;
 using ceres::Solve;
-*/
+
 
 struct Triangle {
 public:
-    int v1i, v2i, v3i, idx;
+    int idx;
+	vector<int> verts;
     Triangle(int v1, int v2, int v3, int id);
 };
 
-Triangle::Triangle(int v1, int v2, int v3, int id) : v1i(v1), v2i(v2), v3i(v3), idx(id) {}
+Triangle::Triangle(int v1, int v2, int v3, int id) {
+	verts.push_back(v1);
+	verts.push_back(v2);
+	verts.push_back(v3);
+	idx = id;
+}
 
 struct Eq1Error {
 public:
     const Triangle tri;
     const int size;
     Eq1Error(const Triangle &_tri, const int &_size) : tri(_tri), size(_size) {};
-	/*
+
     static DynamicAutoDiffCostFunction<Eq1Error>* Create(const Triangle &_tri, const int _size) {
         return (new DynamicAutoDiffCostFunction<Eq1Error>(new Eq1Error(_tri, _size)));
     }
-	*/
+	
     template <typename T>
     bool operator()(T const* const* XYs, T* residuals) const {
         Matrix<T,3,1> v1;
-        v1 << T(XYs[tri.v2i] - XYs[tri.v1i]), T(XYs[tri.v2i + size] - XYs[tri.v1i + size]), T(0);
+        v1 << T(XYs[tri.verts[1]] - XYs[tri.verts[0]]), T(XYs[tri.verts[2] + size] - XYs[tri.verts[0] + size]), T(0);
         Matrix<T,3,1> v2;
-        v2 << T(XYs[tri.v3i] - XYs[tri.v2i]), T(XYs[tri.v3i + size] - XYs[tri.v2i + size]), T(0);
+        v2 << T(XYs[tri.verts[2]] - XYs[tri.verts[2]]), T(XYs[tri.verts[2]+ size] - XYs[tri.verts[1]+ size]), T(0);
 
         Matrix<T,3,1> area = v1.cross(v2);
 
@@ -58,26 +64,46 @@ int main(int argc, char* argv[]) {
     if (loadout) {
         for ( objl::Mesh mesh : Loader.LoadedMeshes ) {
             vector <Triangle*> tris;
-            int i, id;
-            Eigen::Matrix<double, Dynamic, 1> XYs;
+            int i, id;            
 
-            int mesh_size = mesh.Vertices.size();
-
-            XYs.resize(mesh_size*2, 1);
+            int mesh_size = mesh.Vertices.size();           
 
             for (i=0, id=0; i < mesh.Indices.size(); i += 3, id++) {
                 tris.push_back( new Triangle (mesh.Indices[i], mesh.Indices[i+1], mesh.Indices[i+2], id) );
             }
 
-            // MESH TANGLING GOES HERE
-
-
-
-
-
-
-
             // MESH TANGLING
+			double tanglePercent = 0.4;
+			int numTangledTris = (int)tris.size()*tanglePercent;
+
+			for (int i = 0; i < numTangledTris; i++) {
+				int tId = rand() % tris.size(); //random triangle
+				int vId = rand() % 3; //random vertex
+				int dId = rand() % 4; //random direction
+
+				int vnId; //neigbor vertex
+				if (vId != 0) vnId = 0;
+				else if (vId != 1) vnId = 1;
+				else if (vId != 2) vnId = 2;
+
+				double length = sqrt(pow(mesh.Vertices[tris[tId]->verts[vId]].Position.X - mesh.Vertices[tris[tId]->verts[vnId]].Position.X, 2) - pow(mesh.Vertices[tris[tId]->verts[vId]].Position.Y - mesh.Vertices[tris[tId]->verts[vnId]].Position.Y, 2));
+
+				if (dId == 0) {
+					mesh.Vertices[vId].Position.X = mesh.Vertices[vId].Position.X + length * 2;
+				}
+				else if (dId == 1) {
+					mesh.Vertices[vId].Position.X = mesh.Vertices[vId].Position.X - length * 2;
+				}
+				else if (dId == 2) {
+					mesh.Vertices[vId].Position.Y = mesh.Vertices[vId].Position.Y + length * 2;
+				}
+				else if (dId == 3) {
+					mesh.Vertices[vId].Position.Y = mesh.Vertices[vId].Position.Y - length * 2;
+				}
+			}
+
+			Eigen::Matrix<double, Dynamic, 1> XYs;
+			XYs.resize(mesh_size * 2, 1);
 
             i = 0;
             for ( objl::Vertex vertex : mesh.Vertices ) {
@@ -86,7 +112,9 @@ int main(int argc, char* argv[]) {
                 i++;
             }
 
-			/*
+			mesh;
+
+			
             Problem problem;
 
             for ( Triangle* triangle: tris ) {
@@ -96,7 +124,7 @@ int main(int argc, char* argv[]) {
 
                 problem.AddResidualBlock(cost_function, NULL, XYs.data());
             }
-			*/
+			
 
             cout << "here" << endl;
         }
