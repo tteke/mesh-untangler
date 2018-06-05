771ea2214a5abe2cc73eea8ab042184ac7244a6c
Turker Teke <trkrtk@gmail.com>
2018-06-05 13:45:09 +0300

    eq1 implemented(not tested)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 using namespace Eigen;
-using ceres::AutoDiffCostFunction;
+using ceres::DynamicAutoDiffCostFunction;
 using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solver;
@@ -22,18 +22,21 @@ Triangle::Triangle(int v1, int v2, int v3, int id) : v1i(v1), v2i(v2), v3i(v3), 
 struct Eq1Error {
 public:
     const Triangle tri;
-    Eq1Error(const Triangle &_tri) : tri(_tri) {};
-    static CostFunction* Create(const Triangle &_tri, const int &_size) {
-        return (new AutoDiffCostFunction<Eq1Error, 1, _size, _size, _size>(new Eq1Error(_tri)));
+    const int size;
+    Eq1Error(const Triangle &_tri, const int &_size) : tri(_tri), size(_size) {};
+    static DynamicAutoDiffCostFunction<Eq1Error>* Create(const Triangle &_tri, const int _size) {
+        return (new DynamicAutoDiffCostFunction<Eq1Error>(new Eq1Error(_tri, _size)));
     }
     template <typename T>
-    bool operator()(const T* const Xs, const T* const Ys, const T* const Zs, T* residuals) const {
+    bool operator()(T const* const* XYZs, T* residuals) const {
         Matrix<T,3,1> v1;
-        v1 << T(Xs(tri.v2i) - Xs[tri.v1i]), T(Ys[tri.v2i] - Ys[tri.v1i]), T(Zs[tri.v2i] - Zs[tri.v1i]);
+        v1 << T(XYZs[tri.v2i] - XYZs[tri.v1i]), T(XYZs[tri.v2i + size] - XYZs[tri.v1i + size]), T(XYZs[tri.v2i + 2*size] - XYZs[tri.v1i + 2*size]);
         Matrix<T,3,1> v2;
-        v2 << T(Xs(tri.v3i) - Xs[tri.v2i]), T(Ys[tri.v3i] - Ys[tri.v2i]), T(Zs[tri.v3i] - Zs[tri.v2i]);
+        v2 << T(XYZs[tri.v3i] - XYZs[tri.v2i]), T(XYZs[tri.v3i + size] - XYZs[tri.v2i + size]), T(XYZs[tri.v3i + 2*size] - XYZs[tri.v2i + 2*size]);
 
         Matrix<T,3,1> area = v1.cross(v2);
+
+        residuals[0] = T( abs(area[2]) - area[2] );
 
         return true;
     }
@@ -52,24 +55,43 @@ int main(int argc, char* argv[]) {
         for ( objl::Mesh mesh : Loader.LoadedMeshes ) {
             vector <Triangle*> tris;
             int i, id;
-            Eigen::Matrix<float, Dynamic, 1> Xs;
-            Eigen::Matrix<float, Dynamic, 1> Ys;
-            Eigen::Matrix<float, Dynamic, 1> Zs;
+            Eigen::Matrix<double, Dynamic, 1> XYZs;
 
-            Xs.resize(mesh.Vertices.size(), 1);
-            Ys.resize(mesh.Vertices.size(), 1);
-            Zs.resize(mesh.Vertices.size(), 1);
+            int mesh_size = mesh.Vertices.size();
+
+            XYZs.resize(mesh_size*3, 1);
 
             for (i=0, id=0; i < mesh.Indices.size(); i += 3, id++) {
                 tris.push_back( new Triangle (mesh.Indices[i], mesh.Indices[i+1], mesh.Indices[i+2], id) );
             }
 
+            // MESH TANGLING GOES HERE
+
+
+
+
+
+
+
+            // MESH TANGLING
+
             i = 0;
             for ( objl::Vertex vertex : mesh.Vertices ) {
-                Xs(i) = vertex.Position.X;
-                Ys(i) = vertex.Position.Y;
-                Zs(i) = vertex.Position.Z;
+                XYZs(i) = (double)vertex.Position.X;
+                XYZs(i + mesh_size) = (double)vertex.Position.Y;
+                XYZs(i + 2*mesh_size) = (double)vertex.Position.Z;
                 i++;
+            }
+
+
+            Problem problem;
+
+            for ( Triangle* triangle: tris ) {
+                DynamicAutoDiffCostFunction<Eq1Error>* cost_function = Eq1Error::Create(*triangle, mesh_size);
+                cost_function->AddParameterBlock(mesh_size*3);
+                cost_function->SetNumResiduals(1);
+
+                problem.AddResidualBlock(cost_function, NULL, XYZs.data());
             }
 
             cout << "here" << endl;
